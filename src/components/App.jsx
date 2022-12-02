60aa8c1c540c8a3ee9b5b8b62ebf64f8d8887475
rabbani4u <rabbani.iu31@gmail.com>
2022-12-02 12:24:47 +0600

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,22 +3,19 @@ import React from "react";
 var isDone = false;
 
 function strike() {
-  document.getElementById("root").style.textDecoration = "line-through";
+  isDone = true;
 }
+
 function unStrike() {
-  document.getElementById("root").style.textDecoration = null;
+  isDone = false;
 }
 
 function App() {
-  isDone = true;
-
-  // const praStyle = { textDecoration: "line-through" };
-
   return (
     <div>
-      <p>Buy Milk</p>
-      <button onClick={strike}>Click to line-through</button>
-      <button onClick={unStrike}>Click to line-through</button>
+      <p style={isDone ? { textDecoration: "line-through" } : null}>Buy milk</p>
+      <button onClick={strike}>Change to strike through</button>
+      <button onClick={unStrike}>Change back</button>
     </div>
   );
 }
